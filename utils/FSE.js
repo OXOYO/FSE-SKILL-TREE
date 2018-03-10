acb92cbe8fefcac29c8b8bb3ac314c9574d9e66e
OXOYO <zmn2007.hi@163.com>
2018-03-10 14:37:55 +0800

    整理数据。

---
+++ utils/FSE.js
@@ -0,0 +1,272 @@
+/**
+ * Created by OXOYO on 2018/3/10.
+ *
+ * 知识点数据
+ */
+
+export default [
+  {
+    name: 'F2E',
+    title: 'F2E',
+    description: '前端知识点',
+    type: 'catalog',
+    category: '',
+    url: {
+      site: '',
+      github: ''
+    },
+    children: [
+      {
+        name: 'Language',
+        title: '语言',
+        description: '前端语言',
+        type: 'catalog',
+        category: 'language',
+        url: {
+          site: '',
+          github: ''
+        },
+        children: [
+          {
+            name: 'HTML',
+            title: 'HTML',
+            description: '',
+            type: 'catalog',
+            category: 'language',
+            url: {
+              site: '',
+              github: ''
+            },
+            children: [
+              {
+                name: 'HTML4',
+                title: 'HTML4',
+                description: '',
+                type: 'catalog',
+                category: 'language',
+                url: {
+                  site: '',
+                  github: ''
+                },
+                children: [
+
+                ]
+              },
+              {
+                name: 'HTML5',
+                title: 'HTML5',
+                description: '',
+                type: 'catalog',
+                category: 'language',
+                url: {
+                  site: '',
+                  github: ''
+                },
+                children: [
+
+                ]
+              }
+            ]
+          },
+          {
+            name: 'JavaScript',
+            title: 'JavaScript',
+            description: '',
+            type: 'catalog',
+            category: 'language',
+            url: {
+              site: '',
+              github: ''
+            },
+            children: [
+
+            ]
+          },
+          {
+            name: 'TypeScript',
+            title: 'TypeScript',
+            description: '',
+            type: 'catalog',
+            category: 'language',
+            url: {
+              site: '',
+              github: ''
+            },
+            children: [
+
+            ]
+          },
+          {
+            name: 'CSS',
+            title: 'CSS',
+            description: '',
+            type: 'catalog',
+            category: 'language',
+            url: {
+              site: '',
+              github: ''
+            },
+            children: [
+
+            ]
+          }
+        ]
+      },
+      {
+        name: 'debug',
+        title: '调试工具',
+        description: '',
+        type: 'catalog',
+        category: 'debug',
+        url: {
+          site: '',
+          github: ''
+        },
+        children: [
+          {
+            name: 'browser',
+            title: '浏览器',
+            description: '',
+            type: 'catalog',
+            category: 'debug-browser',
+            url: {
+              site: '',
+              github: ''
+            },
+            children: [
+              {
+                name: 'IE',
+                title: 'IE浏览器',
+                description: '',
+                type: 'catalog',
+                category: 'debug-browser',
+                url: {
+                  site: 'https://support.microsoft.com/zh-cn/help/17621/internet-explorer-downloads',
+                  github: ''
+                },
+                children: [
+
+                ]
+              },
+              {
+                name: 'Chrome',
+                title: 'Chrome浏览器',
+                description: '',
+                type: 'catalog',
+                category: 'debug-browser',
+                url: {
+                  site: 'https://www.google.cn/chrome/',
+                  github: ''
+                },
+                children: [
+
+                ]
+              },
+              {
+                name: 'Firefox',
+                title: 'Firefox浏览器',
+                description: '',
+                type: 'catalog',
+                category: 'debug-browser',
+                url: {
+                  site: 'http://www.firefox.com.cn/',
+                  github: ''
+                },
+                children: [
+
+                ]
+              }
+            ]
+          },
+          {
+            name: 'debug-tool',
+            title: '调试工具',
+            description: '',
+            type: 'catalog',
+            category: 'debug-tool',
+            children: [
+              {
+                name: 'Fiddler',
+                title: 'Fiddler',
+                description: '',
+                type: 'catalog',
+                category: 'debug-tool',
+                url: {
+                  site: 'https://www.telerik.com/fiddler',
+                  github: ''
+                },
+                children: [
+
+                ]
+              },
+              {
+                name: 'YSlow',
+                title: 'YSlow',
+                description: '',
+                type: 'catalog',
+                category: 'debug-tool',
+                url: {
+                  site: '',
+                  github: ''
+                },
+                children: [
+
+                ]
+              },
+              {
+                name: 'whistle',
+                title: 'whistle',
+                description: '',
+                type: 'catalog',
+                category: 'debug-tool',
+                url: {
+                  site: '',
+                  github: 'https://github.com/avwo/whistle'
+                },
+                children: [
+
+                ]
+              },
+              {
+                name: 'wireshark',
+                title: 'wireshark',
+                description: '',
+                type: 'catalog',
+                category: 'debug-tool',
+                url: {
+                  site: 'https://www.wireshark.org/',
+                  github: ''
+                },
+                children: [
+
+                ]
+              },
+              {
+                name: 'weinre',
+                title: 'weinre',
+                description: '',
+                type: 'catalog',
+                category: 'debug-tool',
+                url: {
+                  site: 'http://people.apache.org/~pmuellr/weinre/',
+                  github: 'https://www.npmjs.com/package/weinre'
+                },
+                children: [
+
+                ]
+              }
+            ]
+          },
+          {
+            name: 'IDE',
+            title: '编辑器',
+            description: '',
+            type: 'catalog',
+            category: 'develop-tool',
+            children: []
+          }
+        ]
+      }
+    ]
+  }
+]
---
+++ utils/convert.js
@@ -0,0 +1,16 @@
+/**
+ * Created by OXOYO on 2018/3/10.
+ *
+ * 转换工具
+ */
+
+import FSE from './FSE'
+
+const convert = function () {
+  if (!FSE.length) {
+    console.log('No convert data!')
+  }
+}
+
+// 执行转换
+convert()
